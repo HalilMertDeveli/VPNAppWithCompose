a2733767fbb4a2d6c7464d2e4afb5b0a59b639ef
haliko <halilmertdeveliii@gmail.com>
2023-10-09 10:19:16 +0300

    i migrated to scaffold content

--- app/src/main/java/com/example/vpnappclonelikeflutter/view/home_page/exampple.kt
+++ app/src/main/java/com/example/vpnappclonelikeflutter/view/home_page/exampple.kt
@@ -1,11 +1,18 @@
 package com.example.vpnappclonelikeflutter.view.home_page
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -17,8 +24,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
+import com.example.vpnappclonelikeflutter.R
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -88,14 +100,42 @@ fun lan(){
             }
         ) { innerPadding ->
             Column(
-                modifier = Modifier.padding(innerPadding),
+                modifier = Modifier
+                    .padding(innerPadding),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = "Hello world",
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(), contentAlignment = Alignment.Center
+                ) {
+                    val imageRes = painterResource(id = R.drawable.circut)
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+                        Image(
+                            painter = imageRes,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize()
+                        )
 
-                    )
+                    }
+
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            onClick = { println("Connect button is here") },
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .padding(top = 16.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color(0x00FFFFFF))
+                        ) {
+
+                        }
+                    }
+
+
+                }
             }
         }
     }
